4a489ade4d6ebd7657c611ebc3c8580706370070
Sherif <127263385+sizif-22@users.noreply.github.com>
2026-03-23 20:22:34 +0200

    update the auth routes

--- app/sign-in/route.ts
+++ app/sign-in/route.ts
@@ -2,6 +2,6 @@ import { redirect } from 'next/navigation';
 import { getSignInUrl } from '@workos-inc/authkit-nextjs';
 
 export async function GET() {
-  const authorizationUrl = await getSignInUrl();
+  const authorizationUrl = await getSignInUrl({ returnTo: '/console' });
   return redirect(authorizationUrl);
 }
--- app/sign-up/route.ts
+++ app/sign-up/route.ts
@@ -2,6 +2,6 @@ import { redirect } from 'next/navigation';
 import { getSignUpUrl } from '@workos-inc/authkit-nextjs';
 
 export async function GET() {
-  const authorizationUrl = await getSignUpUrl();
+  const authorizationUrl = await getSignUpUrl({ returnTo: '/console' });
   return redirect(authorizationUrl);
 }
